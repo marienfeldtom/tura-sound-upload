b2a22a60a297ad8dbdbd31e2578dba8f2ddcb177
Tom Marienfeld <marienfeldtom@googlemail.com>
2026-01-13 11:41:53 +0000

    changes

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var multer = require('multer');
 var app = express();
 const crypto = require('crypto');
 var path = require('path');
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const low = require('lowdb');
+var fs = require('fs');
+const FileSync = require('lowdb/adapters/FileSync');
 var cors = require('cors');
 app.use(express.json())
 app.use(cors());
@@ -13,24 +14,8 @@ var mustacheExpress = require('mustache-express');
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
-const adapter2 = new FileSync('public/uploads/files.json')
+const adapter2 = new FileSync('db2.json')
 const db2 = low(adapter2)
-
-function isValid(fields) {
-    if (db.get('spieler')
-        .find({ username: fields.username })
-        .value()) {
-            if(db.get('spieler')
-            .find({ username: fields.username })
-            .value().passwort == fields.passwort) {
-                return true;
-            } else {
-                return false;
-            }
-    } else {
-        return false;
-    }
-}
 
 function fileValid(file) {
     if (!file) {
@@ -55,10 +40,7 @@ var storage = multer.diskStorage({
 var upload = multer({
     storage: storage,
     fileFilter: function (req, file, cb) {
-        if (!isValid(req.body)) {
-            return cb(new Error('Falscher Nutzer oder Passwort!'));
-        }
-        if (file.mimetype !== 'audio/mp3') {
+        if (file.mimetype !== 'audio/mpeg') {
             return cb(new Error('Falsches Format'));
         }
         cb(null, true);
@@ -75,36 +57,70 @@ app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
    // res.sendFile(__dirname + "/index.html");
-   db.read();
-   res.render('index.html', {"spieler": db.get('spieler').value()});
+   db2.read();
+   res.render('index.html', {"spieler": db2.get('spieler').value()});
 });
+
+app.get('/delete/:username', function (req, res) {
+    db2.read();
+    db2.get('spieler').remove({username: req.params.username}).write();
+    var mp3path = path.join(__dirname, 'public', 'uploads', req.params.username +'.mp3');
+
+    try {
+        if (fs.existsSync(mp3path)) {
+            fs.unlink(mp3path, function (err) {
+                if (err) throw err;
+                res.redirect("/sound/all/");
+            });
+        } else {
+            res.redirect("/sound/all/");
+        }
+      } catch(err) {
+        console.error(err)
+      }
+
+ });
 
 
   app.get('/info', function (req, res) {
-      res.json(db2.get('spieler').sortBy('anzeigename').value());
+      res.json(db2.get('spieler').sortBy('anzeigename').value().filter(function(element) {
+        return element.version > 0;
+        }));
+});
+
+app.get('/all', function (req, res) {
+    res.render('all.html', {"spieler": db2.get('spieler').value()});
+});
+
+app.get('/edit/:username', function (req, res) {
+    res.render('edit.html', {"spieler": db2.get('spieler').find({ username: req.params.username }).value()});
 });
 
 app.post('/upload', function (req, res) {
-    db.read();
+    db2.read();
    
     upload(req, res, function (err) {
         if (err) return res.status(500).send(err.message);
         if (!req.file) return res.status(500).send("Bitte wähle eine MP3 aus!");
-        var spieler = db.get('spieler').find({ username: req.body.username }).value();
+        var spieler = db2.get('spieler').find({ username: req.body.username }).value();
+        var mannschaft;
+        if(spieler.herren == true) {
+            mannschaft = "herren"
+            } else {
+            mannschaft = "damen"
+        }
         var spieler2 = db2.get('spieler').find({ username: req.body.username }).value();
-        if(!db2.get('spieler').find({ username: req.body.username }).value()) {
-            db2.get('spieler')
-            .push({ username: req.body.username, anzeigename: spieler.anzeigename, version: 1, damen: spieler.damen, herren: spieler.herren})
-            .write()
-        } else {
+        if(db2.get('spieler').find({ username: req.body.username }).value()) {
             db2.find({ username: req.body.username })
             .assign({version: spieler2.version++})
             .write();
+        } else {
+            return res.status(500).send("Spieler wurde nicht gefunden!");
         }
         res.end("File is uploaded");
     });
 });
 
-app.listen(3000,'0.0.0.0', function () {
-    console.log("Working on port 3000");
+app.listen(3005,'0.0.0.0', function () {
+    console.log("Working on port 3005");
 });
